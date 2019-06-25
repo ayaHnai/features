e5ed5d1ec7b4329534528cd33ea68b9ca2597007
AyaAltayeb-FullStack <ayaaltayeb.fullstack@gmail.com>
2019-06-25 16:58:30 +0200

    title

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function(){
+    return(
+        <div>
+            <h3>Design your Own Custom T-Shirt Online With No Minimums</h3>
+            <h4>Enjoy Excellent Quality & Free Shipping </h4>
+        </div>
+    )
+    
+}
