156f450f30c815387057e75b0c2391478c29d303
AyaAltayeb-FullStack <ayaaltayeb.fullstack@gmail.com>
2019-06-25 18:02:23 +0200

    footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,38 +1,37 @@
 import React from 'react'
+import left from '../../images/left.jpg';
 export default function () {
-  return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Shop <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Service</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Journal</a>
-          </li>
-        </ul>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <img src={left} alt="left" />
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Shop <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Service</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Journal</a>
+                    </li>
 
-        <a class="navbar-brand mx-auto" href="#" >UModo<sup>01</sup></a>
-
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Support</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Support</a>
+                    </li>
+                </ul>
+                <a class="nav-link" href="#">Copy Right 2019</a>
+            </div>
+        </nav>
 
 
 
-  )
+    )
 }
