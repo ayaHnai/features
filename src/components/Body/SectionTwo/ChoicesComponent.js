3135e733cc1c4355b3bd16b9384fe0c7469ebb9e
Omnia Yasser <41874132+omniay@users.noreply.github.com>
2019-06-26 12:32:21 +0200

    Add files via upload

--- src/components/Body/SectionTwo/ChoicesComponent.js
+++ src/components/Body/SectionTwo/ChoicesComponent.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import ChoicesComponetTitle from './ChoicesComponetTitle';
 import left from '../../../images/left.jpg';
+import  '../../../styles/choices.css'
+import {FiShoppingCart} from 'react-icons/fi';
+
+import '../../../../node_modules/font-awesome/css/font-awesome.min.css'; 
+
 export default function () {
     return (
         <div>
             <ChoicesComponetTitle />
             <div class="container">
                 <div class="row">
-                    <div class="col">
-                    <img src={left} alt="left" />
+                    <div class="col" >
+                    <div  class="cards">
+                    <img src={left} alt="left"  />
+                    <p>Blablablaaaaaa</p>
+                    <p id='price'>$45</p>
+                    <button class="btn">Shop Now <FiShoppingCart class='s'/></button>
+                     </div>
                      </div>
                     <div class="col">
+                    <div  class="cards">
                             <img src={left} alt="left" />
+                             
+                    <p>Blablablaaaaaa</p>
+                    <p id='price'>$45</p>
+                    <button class="btn">Shop Now </button>
+                    </div>
                     </div>
                     <div class="col">
                     <img src={left} alt="left" />
